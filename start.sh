c8ef52c2ef8141cbb13191ff702bd2ca9455b549
uvidit <uvidit@gmail.com>
2018-05-15 23:35:42 +0300

    changed travis yaml script using bash provider for deploy purposes

--- start.sh
+++ start.sh
@@ -5,6 +5,19 @@ BUCKET_NAME=allure-poc-prjct-bucket
 JAR_NAME=travisMvnGce-1.0-SNAPSHOT-jar-with-dependencies.jar
 VM_NAME=gcedeploy
 
+# Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
+openssl aes-256-cbc
+    -K $encrypted_c5904ac432bd_key
+    -iv $encrypted_c5904ac432bd_iv
+    -in super_secret.txt.enc
+    -out super_secret.txt
+    -d
+
+# If the SDK is not already cached, download it and unpack it
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+   curl https://sdk.cloud.google.com | bash;
+fi
+
 gsutil mb gs://${BUCKET_NAME}
 gsutil cp ./target/${JAR_NAME} gs://${BUCKET_NAME}/${JAR_NAME}
 
