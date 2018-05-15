ef427a51db93f03244712bb48341bb2d82cdedec
uvidit <uvidit@gmail.com>
2018-05-16 00:07:00 +0300

    added some comments

--- start.sh
+++ start.sh
@@ -13,10 +13,13 @@ openssl aes-256-cbc -K $encrypted_c5904ac432bd_key -iv $encrypted_c5904ac432bd_i
 if [ ! -d ${HOME}/google-cloud-sdk ]; then
    curl https://sdk.cloud.google.com | bash;
 fi
+echo "Google SDK downloaded."
+echo "Creating Bucket on Google Storage....."
 
 gsutil mb gs://${BUCKET_NAME}
 gsutil cp ./target/${JAR_NAME} gs://${BUCKET_NAME}/${JAR_NAME}
 
+echo "Creating firewall and starting VM in cloud...."
 gcloud compute firewall-rules create ${VM_NAME}-www --allow tcp:80 --target-tags ${VM_NAME}
 
 gcloud compute instances create ${VM_NAME} \
@@ -24,3 +27,4 @@ gcloud compute instances create ${VM_NAME} \
   --zone us-central1-a  --machine-type n1-standard-1 \
   --metadata-from-file startup-script=install.sh \
   --scopes compute-rw,storage-rw
+echo "done"
